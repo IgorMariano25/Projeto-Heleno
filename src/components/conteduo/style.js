f07e34fa8275e641661dc75492d0ff01aaede2a8
IgorMariano25 <igor-mariano@outlook.com>
2022-06-08 21:12:16 -0300

    Logo SVG das redes sociais, styled Footer e Styled Conteudo

---
+++ src/components/conteduo/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Conteudo = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #000000 ;
+`;
+
+export default Conteudo;
--- src/components/footer/Style.js
+++ src/components/footer/Style.js
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+const Footer = styled.div`
+    bottom: 0%;
+    left:0,033%; /* 36px / 1080px  */
+    width: 100%;
+    height: 0.087%; /* 94px /1080px */
+    background-color: #F5AC00;
+`;
+
+const Logo = styled.img`
+    height: 5em;
+    margin: 0,0138%; /* 15px / 1080px*/
+    margin-left: 7%;
+`;
+
+export { Footer, Logo }
+
