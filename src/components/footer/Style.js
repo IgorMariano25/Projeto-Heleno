05235959c3336b3f9c5e08e4cc33cfbfec95d851
IgorMariano25 <igor-mariano@outlook.com>
2022-06-13 20:47:46 -0300

    Ajuste na position do footer e mudança nas margens do título e P. Além da alteração no tamanho das margens(px para %)

--- src/components/footer/Style.js
+++ src/components/footer/Style.js
@@ -6,19 +6,19 @@ const Footer = styled.footer`
     width: 100%;
     height: 75px; /* 94px /1080px */
     background-color: #F5AC00;
-    position: absolute;
+    position: fixed;
     display: flex;
 `;
 
 const Logo = styled.img`
     height: 3em;
-    margin-right: 0%
+    margin-right: 0%;
     margin: 0,0138%; /* 15px / 1080px*/
     margin-left: 10em;
 `;
 
 const DivRede = styled.div`
-    margin-left: 950px;
+    margin-left: 35%;
 `;
 
 const ListaRede = styled.ul`
