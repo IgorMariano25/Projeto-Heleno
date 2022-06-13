69b05b32a17b520390175e997b0c450444a7bb70
IgorMariano25 <igor-mariano@outlook.com>
2022-06-13 20:42:24 -0300

    Mudança no Width do topo

--- src/components/topo/Style.js
+++ src/components/topo/Style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Card = styled.div`
     top: 0px;
     left: 36px;
-    width: auto;
+    width: 100%;
     height: 75px;
     background-color: #002555;
     display:flex;
