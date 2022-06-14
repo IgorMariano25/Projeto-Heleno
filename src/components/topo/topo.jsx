493c3ff4be1873a78e4b39c0529494f1d503ca3d
IgorMariano25 <igor-mariano@outlook.com>
2022-06-13 21:07:33 -0300

    Removendo os ícones das redes sociais do topo do site

--- src/components/topo/topo.jsx
+++ src/components/topo/topo.jsx
@@ -1,4 +1,4 @@
-import { Card, Img, Textos, DivRede, ListaRede, IconeRedeSocial } from "./Style.js";
+import { Card, Img, Textos } from "./Style.js";
 import {Link} from 'react-router-dom';
 
 const Topo = ( props ) => {
@@ -14,53 +14,6 @@ const Topo = ( props ) => {
                 <Link to="/Alunos">Alunos</Link>
                 <Link to="/Contato">Contato</Link>
             </Textos>
-
-            <DivRede>
-                    <ListaRede>
-                        <li>
-                            <a href="https://www.instagram.com/ibmec/"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/Instagram Amarelo.png` }
-                            alt={ `Instagram do Ibmec` }
-                        /></a>
-                        </li>
-
-                        <li>
-                            <a href="https://twitter.com/Ibmec_oficial"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/Twitter Amarelo.png` }
-                            alt={ `Twitter do Ibmec` }
-                        /></a>
-                        </li>
-
-                        <li>
-                            <a href="https://www.facebook.com/Ibmec/"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/Facebook Amarelo.png` }
-                            alt={ `Facebook do Ibmec` }
-                        /></a>
-                        </li>
-
-                        <li>
-                            <a href="https://br.linkedin.com/school/ibmec"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/LinkedIn Amarelo.png` }
-                            alt={ `LinkedIn do Ibmec` }
-                        /></a>
-                        </li>
-
-                        <li>
-                            <a href="https://api.whatsapp.com/send?phone=5511988593403"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/WhatsApp Amarelo.png` }
-                            alt={ `WhatsApp do Ibmec` }
-                        /></a>
-                        </li>
-
-                        <li>
-                            <a href="https://www.ibmec.br/"><IconeRedeSocial
-                            src={ `Logo Redes Sociais/Globo Amarelo.png` }
-                            alt={ `Site principal do Ibmec` }
-                        /></a>
-                        </li>
-                    </ListaRede>
-                </DivRede>
-
         </Card>
         </nav>
     );
