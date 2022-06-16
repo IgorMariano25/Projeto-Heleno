b1572c7eced9785fa8efd627a86a2451153b166c
IgorMariano25 <igor-mariano@outlook.com>
2022-06-16 11:14:08 -0300

    Remoção das margens das Constantes Titulo, P e SubTitulo )

--- src/components/conteudo/style.js
+++ src/components/conteudo/style.js
@@ -8,8 +8,6 @@ const ConteudoPage = styled.div`
 const Titulo = styled.h1`
     font-family: barlow;
     font-size: 45px;
-    margin-left: 59.3%;
-    margin-bottom: 0.5px;
     width: 434px;
     color: #002555;
     text-align: left;
@@ -20,7 +18,6 @@ const P = styled.p`
     font-style: italic;
     font-size: 20px;
     font-weight: 100px;
-    margin-left: 59.3%;
     width: 434px;
     color: #002555;
     text-align: left;
@@ -30,8 +27,6 @@ const SubTitulo = styled.h2`
     font-family: barlow;
     font-size: 35px;
     color: #002555;
-    margin-left: 500px;
-    margin-top: 45px;
 `;
 
 const Video = styled.video`
