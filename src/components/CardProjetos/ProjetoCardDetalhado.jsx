2d08064acef3cc1352a6527e994e80946d083d7f
IgorMariano25 <igor-mariano@outlook.com>
2022-06-25 09:49:05 -0300

    Componente projeto carda ProjetoCardDetalahdo criado

---
+++ src/components/CardProjetos/ProjetoCardDetalhado.jsx
@@ -0,0 +1,19 @@
+import { Card, Textos, Img, Título, Tecnologias, Participantes } from "./Style";
+
+const ProjetoCard = ( props ) => {
+    return (
+        <Card>
+            <Img
+            src= { `imagens_projetos/${props.fotoProjeto}` }
+            alt={props.título}
+            />
+            <Textos>
+                <Título> {props.titulo} </Título>
+                <Tecnologias>{ props.tecnologias} </Tecnologias>
+                <Participantes>{ props.participantes} </Participantes>
+            </Textos>
+        </Card>
+    )
+}
+
+export default ProjetoCard;
