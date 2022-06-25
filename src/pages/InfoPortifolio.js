23390b9a5fedc6a0c321eb35227811e75e750cad
IgorMariano25 <igor-mariano@outlook.com>
2022-06-25 09:42:15 -0300

    Página detalahda para os portifólios

---
+++ src/pages/InfoPortifolio.js
@@ -0,0 +1,29 @@
+import Topo from "../components/topo/topo";
+import RodaPe from "../components/footer/footer";
+import ProjetoCardDetalhado from "../components/CardProjetos/ProjetoCard";
+import arquivo from "../projetos.json";
+import { useState } from "react";
+
+const ProjetoDetalhado = () => {
+    const [dadosProjeto, setDadosProjeto] = useState(arquivo)
+    const []
+
+    <div>
+        <div className="ContainerCardProjetos">
+            {
+                    dados.map( (p, ind) => (
+                        <ProjetoCard
+                                key = { ind }
+                                fotoProjeto = {p.fotoProjeto}
+                                titulo = {p.titulo}
+                                tecnologias = {p.tecnologias}
+                                participantes = {p.participantes}
+                        />
+                    ))
+                }
+         </div>
+        <RodaPe/>
+    </div>
+}
+
+export default ProjetoDetalhado;
