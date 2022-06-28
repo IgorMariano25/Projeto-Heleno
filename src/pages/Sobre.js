36f34e8fd9df7a8698858fdc09687ab5228d6770
DeveSerOGui <guifelixs@gmail.com>
2022-06-28 17:58:40 -0300

    Criação de um botão na página Sobre

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -12,11 +12,20 @@ const Sobre = () => {
                         Criado por Guilherme Félix, Igor Mariano, Felipe Castelhano e Clarissa Proença.
                     </h2>
                     <h2 className="TituloSobre">
-                        O Projeto Heleno originou-se como um trabalho eletivo de Desenvolvimento Web, matéria curricular dos alunos em Análise e desenvolvimento de sistemas,
-                        e visa facilitar a apresentação dos projetos feitos pelos alunos da instituição Ibmec nos cursos de tecnologia, 
+                        O Projeto Heleno originou-se como um trabalho eletivo de Desenvolvimento Web, matéria curricular dos alunos em Análise e desenvolvimento de sistemas e dirigida por <i>Eduardo Mangeli</i>,
+                        visando facilitar a apresentação dos projetos feitos pelos alunos da instituição Ibmec nos cursos de tecnologia, 
                         tendo como base a apresentação de portifólios dos alunos bem como os resultados alcançados por eles.
                     </h2>
-                    <a href="https://gitlab.com/projeto-multidisciplinar/heleno-app" target="_blank" id="Documentacao">Clique aqui e veja a documentação do projeto.</a>    
+                    <h3 className="InfoSobre">
+                        Para saber mais sobre a documentação, clique aqui e veja o repositório Git.
+                    </h3>
+                        <div className="AlinhamentoBtnSobre">
+                            <a href="https://gitlab.com/projeto-multidisciplinar/heleno-app" target="_blank">
+                                <button id="DocumentacaoBtn">
+                                    Ver documentação
+                                </button>
+                            </a>    
+                        </div>    
                 </div>
             </Conteudo>
             <RodaPe/>
