37c957602e05faf467397dc0b21b8bf9b50c826d
Eduardo Mangeli <eduardo@mangeli.com.br>
2022-06-29 23:11:33 -0300

    browserrouter sem basename

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import InfoPortifolio from "./pages/InfoPortifolio";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ""}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="portfolios" element={<Portfolios />} />
