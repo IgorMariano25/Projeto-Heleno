e38acee466b878702ac1d7630e729f3f6bf96569
Eduardo Mangeli <eduardo@mangeli.com.br>
2022-06-29 23:52:23 -0300

    basename como variável de ambiente própria

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import InfoPortifolio from "./pages/InfoPortifolio";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={ process.env.PUBLIC_URL || ""}>
+    <BrowserRouter basename={ process.env.BASE_NAME || ""}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="portfolios" element={<Portfolios />} />
